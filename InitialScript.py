b32a2f56aec26e5c42773a78acad181d3062ccb9
Felipe Barros <felipe.b4rros@gmail.com>
2019-08-21 11:41:12 -0300

    initial commit

---
+++ InitialScript.py
@@ -0,0 +1,70 @@
+# Import libraries.
+from IPython.display import Image
+import ee
+from pprint import pprint
+
+# Initialize the Earth Engine library.
+ee.Initialize()
+
+image = ee.Image('srtm90_v4')
+pprint(image)
+pprint(image.getInfo())
+
+# Display a thumbnail of a sample image asset.
+Image(url=ee.Image('USGS/SRTMGL1_003').getThumbUrl({'min': 0, 'max': 3000}))
+
+xy = ee.Geometry.Point([86.9250, 27.9881])
+elev = image.sample(xy, 30).first().get('elevation').getInfo()
+print('Mount Everest elevation (m):', elev)
+
+import folium
+
+# Define a method for displaying Earth Engine image tiles to folium map.
+def add_ee_layer(self, eeImageObject, visParams, name):
+  mapID = ee.Image(eeImageObject).getMapId(visParams)
+  folium.raster_layers.TileLayer(
+    tiles = "https://earthengine.googleapis.com/map/"+mapID['mapid']+
+      "/{z}/{x}/{y}?token="+mapID['token'],
+    attr = "Map Data © Google Earth Engine",
+    name = name,
+    overlay = True,
+    control = True
+  ).add_to(self)
+
+# Add EE drawing method to folium.
+folium.Map.add_ee_layer = add_ee_layer
+
+# Set visualization parameters.
+visParams = {'min':0, 'max':3000, 'palette':['225ea8','41b6c4','a1dab4','ffffcc']}
+
+# Create a folium map object.
+myMap = folium.Map(location=[20, 0], zoom_start=3, height=500)
+
+# Add the elevation model to the map object.
+myMap.add_ee_layer(image, visParams, 'DEM')
+
+# Add a layer control panel to the map.
+myMap.add_child(folium.LayerControl())
+
+# Display the map.
+display(myMap)
+
+import matplotlib.pyplot as plt
+# Fetch a Landsat image.
+img = ee.Image('LANDSAT/LT05/C01/T1_SR/LT05_034033_20000913')
+
+# Select Red and NIR bands, scale them, and sample 500 points.
+sampFC = img.select(['B3','B4']).divide(10000).sample(scale=30, numPixels=500)
+
+# Arrange the sample as a list of lists.
+sampDict = sampFC.reduceColumns(ee.Reducer.toList().repeat(2), ['B3', 'B4'])
+sampList = ee.List(sampDict.get('list'))
+
+# Save server-side ee.List as a client-side Python list.
+sampData = sampList.getInfo()
+
+# Display a scatter plot of Red-NIR sample pairs using matplotlib.
+plt.scatter(sampData[0], sampData[1], alpha=0.2)
+plt.xlabel('Red', fontsize=12)
+plt.ylabel('NIR', fontsize=12)
+plt.show()
